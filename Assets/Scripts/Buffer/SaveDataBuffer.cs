7a0fb17561e4ff60aa50e2f384342cfe8562c74e
HyunsuYu <lioie6478@gmail.com>
2026-01-30 19:44:16 +0900

    Init

---
+++ Assets/Scripts/Buffer/SaveDataBuffer.cs
@@ -0,0 +1,81 @@
+using System.IO;
+
+using UnityEngine;
+
+using Newtonsoft.Json;
+
+using CommonUtilLib.ThreadSafe;
+
+
+public sealed class SaveDataBuffer : SingleTonForGameObject<SaveDataBuffer>
+{
+    private SaveData? m_saveData;
+
+    [SerializeField] private string m_fileName = "saveData.data";
+
+
+    public void Awake()
+    {
+        SetInstance(this);
+
+        LoadData();
+    }
+    public void OnDestroy()
+    {
+        Dispose();
+    }
+
+    internal SaveData Data
+    {
+        get
+        {
+            if(!m_saveData.HasValue)
+            {
+                throw new System.Exception("Data is now null");
+            }
+            return m_saveData.Value;
+        }
+    }
+
+    private string DataPath
+    { 
+        get
+        {
+            return Path.Combine(Application.streamingAssetsPath, m_fileName);
+        }
+    }
+
+    internal void LoadData()
+    {
+        if(!File.Exists(DataPath))
+        {
+            m_saveData = new SaveData()
+            {
+                // TODO : Need to implement
+            };
+            SaveData();
+        }
+        else
+        {
+            string jsonData = File.ReadAllText(DataPath);
+            m_saveData = JsonConvert.DeserializeObject<SaveData>(jsonData);
+        }
+    }
+    internal void SaveData()
+    {
+        if(!m_saveData.HasValue)
+        {
+            Debug.LogWarning("SaveDataBuffer.SaveData : Cut data is null");
+            return;
+        }
+
+        string jsonData = JsonConvert.SerializeObject(m_saveData.Value);
+        File.WriteAllText(DataPath, jsonData);
+    }
+
+
+    protected override void Dispose(bool bisDisposing)
+    {
+        m_saveData = null;
+    }
+}
---
+++ Assets/Scripts/Data/SaveData.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+
+public struct SaveData
+{
+    public struct InventoryNode
+    {
+        public ItemType ItemType;
+        public int Count;
+    }
+
+
+    /// <summary>
+    /// 세이브 시점 플레이어 위치
+    /// </summary>
+    public Vector2 PlayerPos;
+
+    /// <summary>
+    /// 인벤토리 아이템 목록. 크기는 반드시 40으로. 실제 UI 배치는 8 * 5로 배치
+    /// </summary>
+    public InventoryNode[] InventoryItems;
+}
+
+internal static class SaveDataExtension
+{
+    public static void AddInventoryItem(this SaveData saveData, in Vector2Int inventoryPos, in ItemType itemType, in int count = 1)
+    {
+        // TODO : Need to implement
+    }
+    public static void MinusInventoryItem(this SaveData saveData, in Vector2Int inventoryPos, in ItemType itemType, in int count = 1)
+    {
+        // TODO : Need to implement
+    }
+    public static void SwapInventoryItem(this SaveData saveData, in Vector2Int pos_0, in Vector2Int pos_1)
+    {
+        // TODO : Need to implement
+    }
+}
